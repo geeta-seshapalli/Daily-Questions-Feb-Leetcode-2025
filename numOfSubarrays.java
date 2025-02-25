6f06cf34d3db96aa896a71f14a694a0e5cac7f99
Geeta Seshapalli <124466507+geeta-seshapalli@users.noreply.github.com>
2025-02-25 19:24:53 +0530

    Feb 25 2025

---
+++ numOfSubarrays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        int n = arr.length;
+        int M = (int)1e9 + 7;
+
+        int count = 0;
+        int odd = 0;
+        int even = 1;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (sum % 2 == 0) { 
+                count = (count + odd) % M;
+                even++;
+            } else { 
+                count = (count + even) % M;
+                odd++;
+            }
+        }
+
+        return count;
+    }
+}
